d48da61f6c6ecf1f59b426990fae06001e8c295a
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-03-16 05:33:01 -0300

    hero fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { BsArrowRightShort } from "react-icons/bs"
 
 export default function Hero() {
     return (
-        <div className="bg-hero-pattern h-[100vh] object-cover bg-cover bg-center w-full">
+        <div className="h-[100vh] w-full" id="hero">
             <div className="container mx-auto flex flex-col justify-center items-center h-[100%] text-center">
                 <h1 className="text-gray-50 text-5xl lg:text-6xl font-bold">Music production from scratch.</h1>
                 <p className="text-gray-50/50 max-w-4xl my-7 text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus voluptatum, qui eos ad ipsum vero earum minima quaerat aliquid, corrupti quis vitae autem vel dolore eligendi? Quibusdam qui placeat nam.</p>
