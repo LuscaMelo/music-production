694e9fd797e29227687682b5cb22a1935c029f37
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-03-16 15:32:59 -0300

    fix

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -7,43 +7,55 @@ import { MdPiano } from "react-icons/md"
 
 export default function Highlights() {
     return (
-        <div className="bg-dark lg:h-[30vh] flex items-center">
+        <div className="bg-dark flex items-center">
             <div className="container mx-auto">
-                <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                    <div className='flex items-center gap-4 p-6 bg-contrast text-gray-50 rounded-md'>
-                        <div className="object-cover w-[400px]">
-                            <img src={img1} alt="" className="rounded-md" />
+                <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                    <div className='bg-contrast'>
+                        <div className="h-[150px]">
+                            <img src={img1} alt="" className="h-[150px] w-full object-cover" />
                         </div>
-                        <div>
-                            <div className="flex items-center gap-1">
+                        <div className="p-5">
+                            <div className="flex items-center gap-2">
                                 <RxMixerHorizontal className="text-brand text-2xl" />
                                 <h1 className="text-xl text-gray-50 font-bold">Mixing</h1>
                             </div>
-                            <p className="text-sm pt-3 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
+                            <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
                         </div>
                     </div>
-                    <div className='flex items-center gap-4 p-6 bg-contrast text-gray-50 rounded-md'>
-                        <div className="object-cover w-[400px]">
-                            <img src={img2} alt="" className="rounded-md" />
+                    <div className='bg-contrast'>
+                        <div className="h-[150px]">
+                            <img src={img2} alt="" className="h-[150px] w-full object-cover" />
                         </div>
-                        <div>
-                            <div className="flex items-center gap-1">
+                        <div className="p-5">
+                            <div className="flex items-center gap-2">
                                 <RxMixerVertical className="text-brand text-2xl" />
                                 <h1 className="text-xl text-gray-50 font-bold">Mastering</h1>
                             </div>
-                            <p className="text-sm pt-3 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
+                            <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
                         </div>
                     </div>
-                    <div className='flex items-center gap-4 p-6 bg-contrast text-gray-50 rounded-md'>
-                        <div className="object-cover w-[400px]">
-                            <img src={img3} alt="" className="rounded-md" />
+                    <div className='bg-contrast'>
+                        <div className="h-[150px]">
+                            <img src={img3} alt="" className="h-[150px] w-full object-cover" />
                         </div>
-                        <div>
-                            <div className="flex items-center gap-1">
-                                <MdPiano className="text-brand text-3xl" />
+                        <div className="p-5">
+                            <div className="flex items-center gap-2">
+                                <MdPiano className="text-brand text-2xl" />
                                 <h1 className="text-xl text-gray-50 font-bold">Recording</h1>
                             </div>
-                            <p className="text-sm pt-3 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
+                            <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
+                        </div>
+                    </div>
+                    <div className='bg-contrast'>
+                        <div className="h-[150px]">
+                            <img src={img3} alt="" className="h-[150px] w-full object-cover" />
+                        </div>
+                        <div className="p-5">
+                            <div className="flex items-center gap-2">
+                                <MdPiano className="text-brand text-2xl" />
+                                <h1 className="text-xl text-gray-50 font-bold">Recording</h1>
+                            </div>
+                            <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae adipisci nisi.</p>
                         </div>
                     </div>
                 </div>
