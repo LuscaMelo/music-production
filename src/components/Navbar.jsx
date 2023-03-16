acce60a9d721c43d8bf362233959ec475ed40112
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-03-16 04:58:53 -0300

    navbar and hero created, first version

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { HiMenuAlt2 } from "react-icons/hi"
+import { IoCloseSharp } from "react-icons/io5"
+
+
+export default function Navbar() {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <div className="flex z-40">
+            {/** Menu Icon **/}
+            <div className="flex items-center justify-center absolute top-4 left-4 cursor-pointer bg-brand text-slate-200 w-[60px] h-[60px] rounded-full" onClick={handleNav}>
+                <HiMenuAlt2 className="text-3xl" />
+            </div>
+
+            {/** Overlay **/}
+            <div className={nav ? "w-full h-full bg-black/80 z-0 absolute top-0 duration-300" : "duration-500"}></div>
+
+            {/** Menu **/}
+            <div className={nav ? "bg-brand absolute top-0 left-0 w-[300px] h-full shadow-lg duration-500" : "absolute top-0 left-0 w-[300px] h-full ml-[-300px] duration-500"}>
+                <div className="">
+                    <div className={nav ? "absolute top-6 right-[-45px]" : ""} >
+                        <IoCloseSharp className="text-4xl cursor-pointer text-slate-100" onClick={handleNav} />
+                    </div>
+                    <div className="h-full w-full ">
+
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/hero.jpg')",
+      },
+      colors: {
+        'brand': '#261D44',
+      },
+    },
+  },
+  plugins: [],
+}
